1103b4cbd9304c55efe0e391dd5f9f200516362f
Jonathan <jonathanrodriguez069@gmail.com>
2024-01-22 23:31:02 -0500

    ...

--- frontend/src/pages/StoreLogIn.jsx
+++ frontend/src/pages/StoreLogIn.jsx
@@ -2,8 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { BsArrowLeft } from 'react-icons/bs';
-import Navbar from "./components/NavBar";
-
+import Navbar from "../components/Navbar";
 
 function StoreLogIn() {
   const [storesData, setStoresData] = useState([])
